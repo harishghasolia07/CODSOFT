1172bd6ae66491ac90ea52d260f4fd07f23f2160
harishghasolia07 <100846446+harishghasolia07@users.noreply.github.com>
2023-08-23 11:07:26 +0530

    Add files via upload

---
+++ Task_3/script.js
@@ -0,0 +1,11 @@
+function clearScreen() {
+    document.getElementById("output").value = "";
+}
+function display(value) {
+    document.getElementById("output").value += value;
+}
+function calculate() {
+    var p = document.getElementById("output").value;
+    var q = eval(p);
+    document.getElementById("output").value = q;
+}
